86c38e3d890f250eefa311c45420e5b36599abf4
Vito Castellano <vito.castellano@facile.it>
2020-06-04 23:09:03 +0200

    aggiunti esempi provati per le implementazioni

---
+++ examples/client/client.go
@@ -0,0 +1,44 @@
+package client
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "bitbucket.org/no-name-game/nn-ws/app/schema/examples/schema"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: "world"})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+
+	response, err := c.SayHelloAgain(ctx, &pb.HelloAgainRequest{
+		Name:    "Vito",
+		Surname: "caste",
+	})
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	log.Printf("Responde: %v", response.GetMessage())
+}
