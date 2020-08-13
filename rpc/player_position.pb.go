3c3c48a199a09c95b0a92d2536a689db2ad311ad
Vito Castellano <vito.castellano@facile.it>
2020-08-13 19:15:24 +0200

    NNWS-35 - Migliorata gestione Titano

--- rpc/player_position.pb.go
+++ rpc/player_position.pb.go
@@ -75,6 +75,85 @@ func (m *PlayerPosition) GetPlanetID() uint32 {
 	return 0
 }
 
+// CreatePlayerPosition
+type CreatePlayerPositionRequest struct {
+	PlayerID             uint32   `protobuf:"varint,1,opt,name=PlayerID,proto3" json:"PlayerID,omitempty"`
+	PlanetID             uint32   `protobuf:"varint,2,opt,name=PlanetID,proto3" json:"PlanetID,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *CreatePlayerPositionRequest) Reset()         { *m = CreatePlayerPositionRequest{} }
+func (m *CreatePlayerPositionRequest) String() string { return proto.CompactTextString(m) }
+func (*CreatePlayerPositionRequest) ProtoMessage()    {}
+func (*CreatePlayerPositionRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_9a8e4d5a504c8881, []int{1}
+}
+
+func (m *CreatePlayerPositionRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_CreatePlayerPositionRequest.Unmarshal(m, b)
+}
+func (m *CreatePlayerPositionRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_CreatePlayerPositionRequest.Marshal(b, m, deterministic)
+}
+func (m *CreatePlayerPositionRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CreatePlayerPositionRequest.Merge(m, src)
+}
+func (m *CreatePlayerPositionRequest) XXX_Size() int {
+	return xxx_messageInfo_CreatePlayerPositionRequest.Size(m)
+}
+func (m *CreatePlayerPositionRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_CreatePlayerPositionRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_CreatePlayerPositionRequest proto.InternalMessageInfo
+
+func (m *CreatePlayerPositionRequest) GetPlayerID() uint32 {
+	if m != nil {
+		return m.PlayerID
+	}
+	return 0
+}
+
+func (m *CreatePlayerPositionRequest) GetPlanetID() uint32 {
+	if m != nil {
+		return m.PlanetID
+	}
+	return 0
+}
+
+type CreatePlayerPositionResponse struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *CreatePlayerPositionResponse) Reset()         { *m = CreatePlayerPositionResponse{} }
+func (m *CreatePlayerPositionResponse) String() string { return proto.CompactTextString(m) }
+func (*CreatePlayerPositionResponse) ProtoMessage()    {}
+func (*CreatePlayerPositionResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_9a8e4d5a504c8881, []int{2}
+}
+
+func (m *CreatePlayerPositionResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_CreatePlayerPositionResponse.Unmarshal(m, b)
+}
+func (m *CreatePlayerPositionResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_CreatePlayerPositionResponse.Marshal(b, m, deterministic)
+}
+func (m *CreatePlayerPositionResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CreatePlayerPositionResponse.Merge(m, src)
+}
+func (m *CreatePlayerPositionResponse) XXX_Size() int {
+	return xxx_messageInfo_CreatePlayerPositionResponse.Size(m)
+}
+func (m *CreatePlayerPositionResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_CreatePlayerPositionResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_CreatePlayerPositionResponse proto.InternalMessageInfo
+
 // GetPlayerCurrentPlanet
 type GetPlayerCurrentPlanetRequest struct {
 	PlayerID             uint32   `protobuf:"varint,1,opt,name=PlayerID,proto3" json:"PlayerID,omitempty"`
@@ -87,7 +166,7 @@ func (m *GetPlayerCurrentPlanetRequest) Reset()         { *m = GetPlayerCurrentP
 func (m *GetPlayerCurrentPlanetRequest) String() string { return proto.CompactTextString(m) }
 func (*GetPlayerCurrentPlanetRequest) ProtoMessage()    {}
 func (*GetPlayerCurrentPlanetRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_9a8e4d5a504c8881, []int{1}
+	return fileDescriptor_9a8e4d5a504c8881, []int{3}
 }
 
 func (m *GetPlayerCurrentPlanetRequest) XXX_Unmarshal(b []byte) error {
@@ -126,7 +205,7 @@ func (m *GetPlayerCurrentPlanetResponse) Reset()         { *m = GetPlayerCurrent
 func (m *GetPlayerCurrentPlanetResponse) String() string { return proto.CompactTextString(m) }
 func (*GetPlayerCurrentPlanetResponse) ProtoMessage()    {}
 func (*GetPlayerCurrentPlanetResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_9a8e4d5a504c8881, []int{2}
+	return fileDescriptor_9a8e4d5a504c8881, []int{4}
 }
 
 func (m *GetPlayerCurrentPlanetResponse) XXX_Unmarshal(b []byte) error {
@@ -166,7 +245,7 @@ func (m *CountPlanetVisitedRequest) Reset()         { *m = CountPlanetVisitedReq
 func (m *CountPlanetVisitedRequest) String() string { return proto.CompactTextString(m) }
 func (*CountPlanetVisitedRequest) ProtoMessage()    {}
 func (*CountPlanetVisitedRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_9a8e4d5a504c8881, []int{3}
+	return fileDescriptor_9a8e4d5a504c8881, []int{5}
 }
 
 func (m *CountPlanetVisitedRequest) XXX_Unmarshal(b []byte) error {
@@ -205,7 +284,7 @@ func (m *CountPlanetVisitedResponse) Reset()         { *m = CountPlanetVisitedRe
 func (m *CountPlanetVisitedResponse) String() string { return proto.CompactTextString(m) }
 func (*CountPlanetVisitedResponse) ProtoMessage()    {}
 func (*CountPlanetVisitedResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_9a8e4d5a504c8881, []int{4}
+	return fileDescriptor_9a8e4d5a504c8881, []int{6}
 }
 
 func (m *CountPlanetVisitedResponse) XXX_Unmarshal(b []byte) error {
@@ -245,7 +324,7 @@ func (m *CountSystemVisitedRequest) Reset()         { *m = CountSystemVisitedReq
 func (m *CountSystemVisitedRequest) String() string { return proto.CompactTextString(m) }
 func (*CountSystemVisitedRequest) ProtoMessage()    {}
 func (*CountSystemVisitedRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_9a8e4d5a504c8881, []int{5}
+	return fileDescriptor_9a8e4d5a504c8881, []int{7}
 }
 
 func (m *CountSystemVisitedRequest) XXX_Unmarshal(b []byte) error {
@@ -284,7 +363,7 @@ func (m *CountSystemVisitedResponse) Reset()         { *m = CountSystemVisitedRe
 func (m *CountSystemVisitedResponse) String() string { return proto.CompactTextString(m) }
 func (*CountSystemVisitedResponse) ProtoMessage()    {}
 func (*CountSystemVisitedResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_9a8e4d5a504c8881, []int{6}
+	return fileDescriptor_9a8e4d5a504c8881, []int{8}
 }
 
 func (m *CountSystemVisitedResponse) XXX_Unmarshal(b []byte) error {
@@ -314,6 +393,8 @@ func (m *CountSystemVisitedResponse) GetValue() uint32 {
 
 func init() {
 	proto.RegisterType((*PlayerPosition)(nil), "player_position.PlayerPosition")
+	proto.RegisterType((*CreatePlayerPositionRequest)(nil), "player_position.CreatePlayerPositionRequest")
+	proto.RegisterType((*CreatePlayerPositionResponse)(nil), "player_position.CreatePlayerPositionResponse")
 	proto.RegisterType((*GetPlayerCurrentPlanetRequest)(nil), "player_position.GetPlayerCurrentPlanetRequest")
 	proto.RegisterType((*GetPlayerCurrentPlanetResponse)(nil), "player_position.GetPlayerCurrentPlanetResponse")
 	proto.RegisterType((*CountPlanetVisitedRequest)(nil), "player_position.CountPlanetVisitedRequest")
@@ -325,21 +406,22 @@ func init() {
 func init() { proto.RegisterFile("rpc/player_position.proto", fileDescriptor_9a8e4d5a504c8881) }
 
 var fileDescriptor_9a8e4d5a504c8881 = []byte{
-	// 242 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x2c, 0x2a, 0x48, 0xd6,
-	0x2f, 0xc8, 0x49, 0xac, 0x4c, 0x2d, 0x8a, 0x2f, 0xc8, 0x2f, 0xce, 0x2c, 0xc9, 0xcc, 0xcf, 0xd3,
-	0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x47, 0x13, 0x96, 0x12, 0x80, 0xaa, 0xcd, 0x4b, 0x2d,
-	0x81, 0x28, 0x51, 0x8a, 0xe0, 0xe2, 0x0b, 0x00, 0x2b, 0x0a, 0x80, 0xaa, 0x11, 0xe2, 0xe3, 0x62,
-	0xf2, 0x74, 0x91, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x0d, 0x62, 0xf2, 0x74, 0x11, 0x92, 0xe2, 0xe2,
-	0x80, 0xa8, 0xf0, 0x74, 0x91, 0x60, 0x02, 0x8b, 0xc2, 0xf9, 0x50, 0xb9, 0xbc, 0xd4, 0x12, 0x4f,
-	0x17, 0x09, 0x66, 0xb8, 0x1c, 0x98, 0xaf, 0x64, 0xcd, 0x25, 0xeb, 0x9e, 0x5a, 0x02, 0x51, 0xea,
-	0x5c, 0x5a, 0x54, 0x94, 0x9a, 0x57, 0x02, 0x91, 0x0b, 0x4a, 0x2d, 0x2c, 0x4d, 0x2d, 0x2e, 0x41,
-	0x31, 0x98, 0x11, 0xd5, 0x60, 0x25, 0x0f, 0x2e, 0x39, 0x5c, 0x9a, 0x8b, 0x0b, 0xf2, 0xf3, 0x8a,
-	0x53, 0x85, 0xd4, 0xb8, 0xd8, 0x20, 0x22, 0x60, 0xbd, 0xdc, 0x46, 0x7c, 0x7a, 0x50, 0x7f, 0x41,
-	0xd5, 0x41, 0x65, 0x95, 0xcc, 0xb9, 0x24, 0x9d, 0xf3, 0x4b, 0x61, 0xda, 0xc3, 0x32, 0x8b, 0x33,
-	0x4b, 0x52, 0x53, 0x88, 0x71, 0x82, 0x11, 0x97, 0x14, 0x36, 0x8d, 0x50, 0xeb, 0x45, 0xb8, 0x58,
-	0xc3, 0x12, 0x73, 0x4a, 0x53, 0xa1, 0xda, 0x20, 0x1c, 0xb8, 0x65, 0xc1, 0x95, 0xc5, 0x25, 0xa9,
-	0xb9, 0x64, 0x58, 0x86, 0xa6, 0x11, 0x9f, 0x65, 0x4e, 0xac, 0x51, 0xcc, 0x45, 0x05, 0xc9, 0x49,
-	0x6c, 0xe0, 0x88, 0x34, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0xbe, 0x1e, 0xd8, 0x70, 0x08, 0x02,
-	0x00, 0x00,
+	// 266 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x9c, 0x92, 0x41, 0x4b, 0xc3, 0x40,
+	0x10, 0x85, 0x49, 0x4a, 0x8b, 0x8c, 0x18, 0x25, 0x78, 0x48, 0xa3, 0x16, 0xd9, 0x83, 0x78, 0xaa,
+	0x50, 0x0f, 0x1e, 0xbc, 0x99, 0x80, 0xe6, 0x56, 0x2a, 0x16, 0xf1, 0x22, 0xb1, 0xce, 0x21, 0x50,
+	0x77, 0xd7, 0xdd, 0xd9, 0x43, 0xff, 0xbd, 0xb0, 0x3b, 0x06, 0x1a, 0x6a, 0x09, 0x1e, 0xe7, 0xbd,
+	0xf7, 0xbd, 0x17, 0xc2, 0xc2, 0xd8, 0xe8, 0xd5, 0x8d, 0x5e, 0xd7, 0x1b, 0x34, 0xef, 0x5a, 0xd9,
+	0x86, 0x1a, 0x25, 0xa7, 0xda, 0x28, 0x52, 0xe9, 0x71, 0x47, 0xce, 0x4f, 0x38, 0x2b, 0x91, 0x42,
+	0x44, 0xbc, 0x42, 0x32, 0xf7, 0xa1, 0x39, 0x67, 0xd2, 0x04, 0xe2, 0xaa, 0xcc, 0xa2, 0xcb, 0xe8,
+	0xfa, 0x68, 0x11, 0x57, 0x65, 0x9a, 0xc3, 0x41, 0x48, 0x54, 0x65, 0x16, 0x7b, 0xb5, 0xbd, 0xd9,
+	0x93, 0x48, 0x55, 0x99, 0x0d, 0x5a, 0xcf, 0xdf, 0xe2, 0x05, 0xce, 0x0a, 0x83, 0x35, 0xe1, 0x76,
+	0xff, 0x02, 0xbf, 0x1d, 0x5a, 0xda, 0xaa, 0x8d, 0xf6, 0xd4, 0xc6, 0x9d, 0xda, 0x09, 0x9c, 0xef,
+	0xae, 0xb5, 0x5a, 0x49, 0x8b, 0xe2, 0x1e, 0x2e, 0x1e, 0x91, 0x82, 0x59, 0x38, 0x63, 0x50, 0x52,
+	0x60, 0x7b, 0x0c, 0x8b, 0x27, 0x98, 0xfc, 0x05, 0x87, 0xfa, 0xf4, 0x0a, 0x46, 0x41, 0xf1, 0xec,
+	0xe1, 0x2c, 0x99, 0xf2, 0xef, 0xe4, 0x1c, 0xbb, 0xe2, 0x0e, 0xc6, 0x85, 0x72, 0xbf, 0xf8, 0xb2,
+	0xb1, 0x0d, 0xe1, 0x67, 0x9f, 0x4f, 0x98, 0x41, 0xbe, 0x0b, 0xe4, 0xf9, 0x53, 0x18, 0x2e, 0xeb,
+	0xb5, 0x43, 0xc6, 0xc2, 0xd1, 0x8e, 0x3d, 0x6f, 0x2c, 0xe1, 0xd7, 0x3f, 0xc6, 0x3a, 0xe0, 0xbe,
+	0xb1, 0x87, 0xe1, 0xdb, 0xc0, 0xe8, 0xd5, 0xc7, 0xc8, 0xbf, 0x9f, 0xdb, 0x9f, 0x00, 0x00, 0x00,
+	0xff, 0xff, 0xc7, 0x79, 0xd6, 0x8a, 0x7f, 0x02, 0x00, 0x00,
 }
