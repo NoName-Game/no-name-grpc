ac0874abfb35a35bccb6ff641b660c47ce63ab10
Vito Castellano <vito.castellano@facile.it>
2021-02-28 17:39:46 +0100

    NNT-208 - Aggiunta gestione notifiche

--- build/pb/achievement.pb.go
+++ build/pb/achievement.pb.go
@@ -211,6 +211,95 @@ func (m *GetAllAchievementResponse) GetAchievements() []*Achievement {
 	return nil
 }
 
+// GetAchievementByID
+type GetAchievementByIDRequest struct {
+	AchievementID uint32 `protobuf:"varint,1,opt,name=AchievementID,proto3" json:"AchievementID,omitempty"`
+}
+
+func (m *GetAchievementByIDRequest) Reset()         { *m = GetAchievementByIDRequest{} }
+func (m *GetAchievementByIDRequest) String() string { return proto.CompactTextString(m) }
+func (*GetAchievementByIDRequest) ProtoMessage()    {}
+func (*GetAchievementByIDRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_d4732ecd86297bb9, []int{3}
+}
+func (m *GetAchievementByIDRequest) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *GetAchievementByIDRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_GetAchievementByIDRequest.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *GetAchievementByIDRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GetAchievementByIDRequest.Merge(m, src)
+}
+func (m *GetAchievementByIDRequest) XXX_Size() int {
+	return m.Size()
+}
+func (m *GetAchievementByIDRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_GetAchievementByIDRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_GetAchievementByIDRequest proto.InternalMessageInfo
+
+func (m *GetAchievementByIDRequest) GetAchievementID() uint32 {
+	if m != nil {
+		return m.AchievementID
+	}
+	return 0
+}
+
+type GetAchievementByIDResponse struct {
+	Achievement *Achievement `protobuf:"bytes,1,opt,name=Achievement,proto3" json:"Achievement,omitempty"`
+}
+
+func (m *GetAchievementByIDResponse) Reset()         { *m = GetAchievementByIDResponse{} }
+func (m *GetAchievementByIDResponse) String() string { return proto.CompactTextString(m) }
+func (*GetAchievementByIDResponse) ProtoMessage()    {}
+func (*GetAchievementByIDResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_d4732ecd86297bb9, []int{4}
+}
+func (m *GetAchievementByIDResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *GetAchievementByIDResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_GetAchievementByIDResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *GetAchievementByIDResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GetAchievementByIDResponse.Merge(m, src)
+}
+func (m *GetAchievementByIDResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *GetAchievementByIDResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_GetAchievementByIDResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_GetAchievementByIDResponse proto.InternalMessageInfo
+
+func (m *GetAchievementByIDResponse) GetAchievement() *Achievement {
+	if m != nil {
+		return m.Achievement
+	}
+	return nil
+}
+
 // GetAchievementsByCategoryID
 type GetAchievementsByCategoryIDRequest struct {
 	CategoryID uint32 `protobuf:"varint,1,opt,name=CategoryID,proto3" json:"CategoryID,omitempty"`
@@ -220,7 +309,7 @@ func (m *GetAchievementsByCategoryIDRequest) Reset()         { *m = GetAchieveme
 func (m *GetAchievementsByCategoryIDRequest) String() string { return proto.CompactTextString(m) }
 func (*GetAchievementsByCategoryIDRequest) ProtoMessage()    {}
 func (*GetAchievementsByCategoryIDRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_d4732ecd86297bb9, []int{3}
+	return fileDescriptor_d4732ecd86297bb9, []int{5}
 }
 func (m *GetAchievementsByCategoryIDRequest) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -264,7 +353,7 @@ func (m *GetAchievementsByCategoryIDResponse) Reset()         { *m = GetAchievem
 func (m *GetAchievementsByCategoryIDResponse) String() string { return proto.CompactTextString(m) }
 func (*GetAchievementsByCategoryIDResponse) ProtoMessage()    {}
 func (*GetAchievementsByCategoryIDResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_d4732ecd86297bb9, []int{4}
+	return fileDescriptor_d4732ecd86297bb9, []int{6}
 }
 func (m *GetAchievementsByCategoryIDResponse) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -312,7 +401,7 @@ func (m *GetAchievementForPlayerByCategoryRequest) Reset() {
 func (m *GetAchievementForPlayerByCategoryRequest) String() string { return proto.CompactTextString(m) }
 func (*GetAchievementForPlayerByCategoryRequest) ProtoMessage()    {}
 func (*GetAchievementForPlayerByCategoryRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_d4732ecd86297bb9, []int{5}
+	return fileDescriptor_d4732ecd86297bb9, []int{7}
 }
 func (m *GetAchievementForPlayerByCategoryRequest) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -367,7 +456,7 @@ func (m *GetAchievementForPlayerByCategoryResponse) String() string {
 }
 func (*GetAchievementForPlayerByCategoryResponse) ProtoMessage() {}
 func (*GetAchievementForPlayerByCategoryResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_d4732ecd86297bb9, []int{6}
+	return fileDescriptor_d4732ecd86297bb9, []int{8}
 }
 func (m *GetAchievementForPlayerByCategoryResponse) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -413,7 +502,7 @@ func (m *CheckIfPlayerHaveAchievementRequest) Reset()         { *m = CheckIfPlay
 func (m *CheckIfPlayerHaveAchievementRequest) String() string { return proto.CompactTextString(m) }
 func (*CheckIfPlayerHaveAchievementRequest) ProtoMessage()    {}
 func (*CheckIfPlayerHaveAchievementRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_d4732ecd86297bb9, []int{7}
+	return fileDescriptor_d4732ecd86297bb9, []int{9}
 }
 func (m *CheckIfPlayerHaveAchievementRequest) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -465,7 +554,7 @@ func (m *CheckIfPlayerHaveAchievementResponse) Reset()         { *m = CheckIfPla
 func (m *CheckIfPlayerHaveAchievementResponse) String() string { return proto.CompactTextString(m) }
 func (*CheckIfPlayerHaveAchievementResponse) ProtoMessage()    {}
 func (*CheckIfPlayerHaveAchievementResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_d4732ecd86297bb9, []int{8}
+	return fileDescriptor_d4732ecd86297bb9, []int{10}
 }
 func (m *CheckIfPlayerHaveAchievementResponse) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -512,6 +601,8 @@ func init() {
 	proto.RegisterType((*Achievement)(nil), "achievement.Achievement")
 	proto.RegisterType((*GetAllAchievementRequest)(nil), "achievement.GetAllAchievementRequest")
 	proto.RegisterType((*GetAllAchievementResponse)(nil), "achievement.GetAllAchievementResponse")
+	proto.RegisterType((*GetAchievementByIDRequest)(nil), "achievement.GetAchievementByIDRequest")
+	proto.RegisterType((*GetAchievementByIDResponse)(nil), "achievement.GetAchievementByIDResponse")
 	proto.RegisterType((*GetAchievementsByCategoryIDRequest)(nil), "achievement.GetAchievementsByCategoryIDRequest")
 	proto.RegisterType((*GetAchievementsByCategoryIDResponse)(nil), "achievement.GetAchievementsByCategoryIDResponse")
 	proto.RegisterType((*GetAchievementForPlayerByCategoryRequest)(nil), "achievement.GetAchievementForPlayerByCategoryRequest")
@@ -523,37 +614,38 @@ func init() {
 func init() { proto.RegisterFile("proto/achievement.proto", fileDescriptor_d4732ecd86297bb9) }
 
 var fileDescriptor_d4732ecd86297bb9 = []byte{
-	// 468 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x54, 0x41, 0x6f, 0xd3, 0x30,
-	0x14, 0xae, 0xd3, 0x6e, 0x74, 0xaf, 0x14, 0x90, 0x11, 0xc2, 0xf4, 0x60, 0x45, 0x5e, 0x0f, 0x1e,
-	0x87, 0x22, 0x0d, 0x4e, 0xc0, 0x65, 0x5b, 0x60, 0xf4, 0x82, 0xc0, 0x9c, 0x80, 0x03, 0xf2, 0xb2,
-	0x47, 0x5b, 0x91, 0x26, 0x25, 0x4d, 0x07, 0x95, 0xf8, 0x09, 0x1c, 0xf8, 0x59, 0x1c, 0x77, 0xe4,
-	0x88, 0x5a, 0x89, 0xdf, 0x81, 0xea, 0x64, 0xa9, 0xb3, 0x86, 0x08, 0x24, 0x6e, 0xf6, 0xf7, 0x7d,
-	0x7e, 0xef, 0xfb, 0xfc, 0x12, 0xc3, 0xed, 0x49, 0x1c, 0x25, 0xd1, 0x3d, 0xed, 0x0f, 0x47, 0x78,
-	0x86, 0x63, 0x0c, 0x93, 0x9e, 0x41, 0x68, 0xcb, 0x82, 0x3a, 0xee, 0x86, 0xea, 0x9d, 0xaf, 0x13,
-	0x1c, 0x44, 0xf1, 0x3c, 0x95, 0x8b, 0x5f, 0x0e, 0xb4, 0x0e, 0xd6, 0x34, 0xbd, 0x06, 0x4e, 0xdf,
-	0x63, 0xc4, 0x25, 0xb2, 0xad, 0x9c, 0xbe, 0x47, 0x29, 0x34, 0x9e, 0xeb, 0x31, 0x32, 0xc7, 0x25,
-	0x72, 0x47, 0x99, 0xf5, 0x0a, 0x7b, 0x15, 0xcc, 0x06, 0xac, 0x9e, 0x62, 0xab, 0x35, 0xed, 0x40,
-	0xf3, 0xe5, 0x4c, 0x87, 0xc9, 0x28, 0x99, 0xb3, 0x86, 0x4b, 0x64, 0x5d, 0xe5, 0x7b, 0xca, 0x01,
-	0x8e, 0xa3, 0xe0, 0x54, 0xe1, 0x27, 0x1d, 0x9f, 0xb2, 0x2d, 0x97, 0xc8, 0x2d, 0x65, 0x21, 0xb4,
-	0x0b, 0x6d, 0x6f, 0xa4, 0xc7, 0x51, 0x78, 0x21, 0xd9, 0x36, 0x92, 0x22, 0x48, 0xef, 0xc2, 0x8d,
-	0x27, 0x9f, 0x27, 0x18, 0x8f, 0x30, 0xf4, 0x31, 0x13, 0x5e, 0x31, 0xc2, 0x0d, 0x9c, 0x3e, 0x80,
-	0x5b, 0x56, 0xa8, 0xa3, 0x2c, 0x72, 0xdf, 0x63, 0x4d, 0x13, 0xac, 0x9c, 0xa4, 0x6f, 0xe1, 0x66,
-	0x09, 0xc1, 0x76, 0x5c, 0x22, 0x5b, 0xfb, 0x7b, 0xbd, 0xd2, 0x5b, 0x2c, 0x39, 0xa0, 0xca, 0xaa,
-	0x88, 0x0e, 0xb0, 0x63, 0x4c, 0x0e, 0x82, 0xc0, 0x22, 0x15, 0x7e, 0x9c, 0xe1, 0x34, 0x11, 0xaf,
-	0xe1, 0x4e, 0x09, 0x37, 0x9d, 0x44, 0xe1, 0x14, 0xe9, 0x63, 0xb8, 0x6a, 0xc1, 0x53, 0x46, 0xdc,
-	0xba, 0x6c, 0xed, 0x33, 0xdb, 0x8e, 0xed, 0x42, 0x15, 0xd4, 0xc2, 0x03, 0xb1, 0x2a, 0x6d, 0x41,
-	0x87, 0xf3, 0x75, 0xe4, 0xcc, 0xc0, 0x6a, 0x42, 0xd6, 0x25, 0xa5, 0xd3, 0xb7, 0x10, 0xe1, 0xc3,
-	0x6e, 0x65, 0x95, 0xff, 0x62, 0xf5, 0x0b, 0xc8, 0x62, 0x93, 0xa7, 0x51, 0xfc, 0x22, 0xd0, 0x73,
-	0x8c, 0xd7, 0xcd, 0x2e, 0x0c, 0x77, 0xa0, 0x99, 0x52, 0xb9, 0xdd, 0x7c, 0xff, 0xe7, 0xe1, 0x3b,
-	0x15, 0xc3, 0x17, 0x03, 0xd8, 0xfb, 0x8b, 0xee, 0x59, 0xd0, 0x87, 0x85, 0x9f, 0xc6, 0x38, 0xa8,
-	0xca, 0x69, 0x8b, 0xc5, 0x00, 0x76, 0x8f, 0x86, 0xe8, 0x7f, 0xe8, 0xbf, 0x4f, 0xcb, 0x3f, 0xd3,
-	0x67, 0xb8, 0xf9, 0x4d, 0x54, 0x26, 0xec, 0x42, 0xdb, 0x3a, 0x91, 0x27, 0x2b, 0x82, 0xe2, 0x2b,
-	0x81, 0x6e, 0x75, 0xa7, 0x2c, 0x8d, 0x84, 0xeb, 0xc3, 0x22, 0x65, 0x3a, 0x36, 0xd5, 0x65, 0xf8,
-	0x72, 0x6e, 0xe7, 0x1f, 0x72, 0x1f, 0xf2, 0xef, 0x0b, 0x4e, 0xce, 0x17, 0x9c, 0xfc, 0x5c, 0x70,
-	0xf2, 0x6d, 0xc9, 0x6b, 0xe7, 0x4b, 0x5e, 0xfb, 0xb1, 0xe4, 0xb5, 0x37, 0x8d, 0xde, 0xa3, 0xc9,
-	0xc9, 0xc9, 0xb6, 0x79, 0x90, 0xee, 0xff, 0x0e, 0x00, 0x00, 0xff, 0xff, 0x70, 0xf9, 0x8b, 0xf3,
-	0xda, 0x04, 0x00, 0x00,
+	// 491 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x54, 0x4d, 0x6f, 0xd3, 0x40,
+	0x10, 0xcd, 0x3a, 0x69, 0x49, 0x27, 0x14, 0xd0, 0x22, 0xc4, 0xe2, 0xc3, 0xca, 0xda, 0xf6, 0xe0,
+	0x72, 0x08, 0x52, 0xe1, 0x04, 0x5c, 0x92, 0x1a, 0x8a, 0x2f, 0x08, 0x96, 0x0b, 0x1f, 0x07, 0xe4,
+	0xba, 0x43, 0x12, 0xe1, 0xd8, 0xc1, 0x76, 0x0a, 0x96, 0xf8, 0x09, 0x1c, 0xf8, 0x59, 0x1c, 0x7b,
+	0xe4, 0x88, 0x12, 0x89, 0xdf, 0x81, 0x62, 0x3b, 0xc9, 0xba, 0x59, 0x2c, 0x90, 0x7a, 0xdb, 0x7d,
+	0xef, 0x79, 0xe6, 0xbd, 0x9d, 0x91, 0xe1, 0xf6, 0x24, 0x8e, 0xd2, 0xe8, 0x9e, 0xe7, 0x0f, 0x47,
+	0x78, 0x86, 0x63, 0x0c, 0xd3, 0x6e, 0x8e, 0xd0, 0x8e, 0x02, 0x99, 0xd6, 0x86, 0xea, 0xbd, 0xef,
+	0xa5, 0x38, 0x88, 0xe2, 0xac, 0x90, 0x8b, 0xdf, 0x06, 0x74, 0x7a, 0x6b, 0x9a, 0x5e, 0x03, 0xc3,
+	0x75, 0x18, 0xb1, 0x88, 0xbd, 0x2b, 0x0d, 0xd7, 0xa1, 0x14, 0x5a, 0xcf, 0xbd, 0x31, 0x32, 0xc3,
+	0x22, 0xf6, 0x8e, 0xcc, 0xcf, 0x0b, 0xec, 0x55, 0x30, 0x1d, 0xb0, 0x66, 0x81, 0x2d, 0xce, 0xd4,
+	0x84, 0xf6, 0xcb, 0xa9, 0x17, 0xa6, 0xa3, 0x34, 0x63, 0x2d, 0x8b, 0xd8, 0x4d, 0xb9, 0xba, 0x53,
+	0x0e, 0x70, 0x1c, 0x05, 0xa7, 0x12, 0x3f, 0x7b, 0xf1, 0x29, 0xdb, 0xb2, 0x88, 0xbd, 0x25, 0x15,
+	0x84, 0xee, 0xc3, 0xae, 0x33, 0xf2, 0xc6, 0x51, 0xb8, 0x94, 0x6c, 0xe7, 0x92, 0x2a, 0x48, 0xef,
+	0xc2, 0x8d, 0x27, 0x5f, 0x26, 0x18, 0x8f, 0x30, 0xf4, 0xb1, 0x14, 0x5e, 0xc9, 0x85, 0x1b, 0x38,
+	0x7d, 0x00, 0xb7, 0x94, 0x50, 0x47, 0x65, 0x64, 0xd7, 0x61, 0xed, 0x3c, 0x98, 0x9e, 0xa4, 0xef,
+	0xe0, 0xa6, 0x86, 0x60, 0x3b, 0x16, 0xb1, 0x3b, 0x87, 0x07, 0x5d, 0xed, 0x2b, 0x6a, 0x3e, 0x90,
+	0xba, 0x2a, 0xc2, 0x04, 0x76, 0x8c, 0x69, 0x2f, 0x08, 0x14, 0x52, 0xe2, 0xa7, 0x29, 0x26, 0xa9,
+	0x78, 0x03, 0x77, 0x34, 0x5c, 0x32, 0x89, 0xc2, 0x04, 0xe9, 0x63, 0xb8, 0xaa, 0xc0, 0x09, 0x23,
+	0x56, 0xd3, 0xee, 0x1c, 0x32, 0xd5, 0x8e, 0xea, 0x42, 0x56, 0xd4, 0xa2, 0x57, 0x94, 0x5e, 0x43,
+	0xfd, 0xcc, 0x75, 0xca, 0xbe, 0x8b, 0x87, 0x57, 0x98, 0xd5, 0xdc, 0xab, 0xa0, 0x78, 0x0d, 0xa6,
+	0xae, 0x44, 0x69, 0xef, 0x61, 0x65, 0x7f, 0xf2, 0x0a, 0x75, 0xee, 0x54, 0xb1, 0x70, 0x40, 0x54,
+	0x2b, 0x27, 0xfd, 0x6c, 0x3d, 0x8f, 0xa5, 0x4b, 0x0e, 0xa0, 0x4c, 0xb0, 0xb0, 0xa8, 0x20, 0xc2,
+	0x87, 0xbd, 0xda, 0x2a, 0x97, 0xf2, 0x8e, 0x5f, 0xc1, 0xae, 0x36, 0x79, 0x1a, 0xc5, 0x2f, 0x02,
+	0x2f, 0xc3, 0x78, 0xdd, 0x6c, 0x69, 0xd8, 0x84, 0x76, 0x41, 0xad, 0xec, 0xae, 0xee, 0x7f, 0xdf,
+	0x4c, 0xa3, 0x66, 0x33, 0xc5, 0x00, 0x0e, 0xfe, 0xa1, 0xfb, 0x25, 0x4c, 0x64, 0x00, 0x7b, 0x47,
+	0x43, 0xf4, 0x3f, 0xba, 0x1f, 0x8a, 0xf2, 0xcf, 0xbc, 0x33, 0xdc, 0x5c, 0xd8, 0xda, 0x84, 0x1b,
+	0x4b, 0x65, 0xe8, 0x96, 0xea, 0x1b, 0x81, 0xfd, 0xfa, 0x4e, 0x65, 0x1a, 0x1b, 0xae, 0x0f, 0xab,
+	0x54, 0xde, 0xb1, 0x2d, 0x2f, 0xc2, 0x17, 0x73, 0x1b, 0xff, 0x91, 0xbb, 0xcf, 0x7f, 0xcc, 0x38,
+	0x39, 0x9f, 0x71, 0xf2, 0x6b, 0xc6, 0xc9, 0xf7, 0x39, 0x6f, 0x9c, 0xcf, 0x79, 0xe3, 0xe7, 0x9c,
+	0x37, 0xde, 0xb6, 0xba, 0x8f, 0x26, 0x27, 0x27, 0xdb, 0xf9, 0xdf, 0xf2, 0xfe, 0x9f, 0x00, 0x00,
+	0x00, 0xff, 0xff, 0x92, 0x1a, 0xdc, 0xc8, 0x77, 0x05, 0x00, 0x00,
 }
 
 func (m *Achievement) Marshal() (dAtA []byte, err error) {
@@ -691,6 +783,69 @@ func (m *GetAllAchievementResponse) MarshalToSizedBuffer(dAtA []byte) (int, erro
 			i--
 			dAtA[i] = 0xa
 		}
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *GetAchievementByIDRequest) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *GetAchievementByIDRequest) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *GetAchievementByIDRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.AchievementID != 0 {
+		i = encodeVarintAchievement(dAtA, i, uint64(m.AchievementID))
+		i--
+		dAtA[i] = 0x8
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *GetAchievementByIDResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *GetAchievementByIDResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *GetAchievementByIDResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.Achievement != nil {
+		{
+			size, err := m.Achievement.MarshalToSizedBuffer(dAtA[:i])
+			if err != nil {
+				return 0, err
+			}
+			i -= size
+			i = encodeVarintAchievement(dAtA, i, uint64(size))
+		}
+		i--
+		dAtA[i] = 0xa
 	}
 	return len(dAtA) - i, nil
 }
@@ -976,6 +1131,31 @@ func (m *GetAllAchievementResponse) Size() (n int) {
 			l = e.Size()
 			n += 1 + l + sovAchievement(uint64(l))
 		}
+	}
+	return n
+}
+
+func (m *GetAchievementByIDRequest) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if m.AchievementID != 0 {
+		n += 1 + sovAchievement(uint64(m.AchievementID))
+	}
+	return n
+}
+
+func (m *GetAchievementByIDResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if m.Achievement != nil {
+		l = m.Achievement.Size()
+		n += 1 + l + sovAchievement(uint64(l))
 	}
 	return n
 }
@@ -1452,6 +1632,167 @@ func (m *GetAllAchievementResponse) Unmarshal(dAtA []byte) error {
 			}
 			m.Achievements = append(m.Achievements, &Achievement{})
 			if err := m.Achievements[len(m.Achievements)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipAchievement(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthAchievement
+			}
+			if (iNdEx + skippy) < 0 {
+				return ErrInvalidLengthAchievement
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *GetAchievementByIDRequest) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowAchievement
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: GetAchievementByIDRequest: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: GetAchievementByIDRequest: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field AchievementID", wireType)
+			}
+			m.AchievementID = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowAchievement
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.AchievementID |= uint32(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		default:
+			iNdEx = preIndex
+			skippy, err := skipAchievement(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthAchievement
+			}
+			if (iNdEx + skippy) < 0 {
+				return ErrInvalidLengthAchievement
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *GetAchievementByIDResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowAchievement
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: GetAchievementByIDResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: GetAchievementByIDResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Achievement", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowAchievement
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthAchievement
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthAchievement
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.Achievement == nil {
+				m.Achievement = &Achievement{}
+			}
+			if err := m.Achievement.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
